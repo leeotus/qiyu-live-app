9ce5affa24896dc6f5177993e603a92107489fb3
leeotus <leeotus@163.com>
2026-02-12 13:47:05 +0800

    user tag rpc implement

--- qiyu-live-user-provider/src/main/java/org/idea/live/user/provider/rpc/UserTagRpcImpl.java
+++ qiyu-live-user-provider/src/main/java/org/idea/live/user/provider/rpc/UserTagRpcImpl.java
@@ -1,26 +1,31 @@
 package org.idea.live.user.provider.rpc;
 
+import org.apache.dubbo.config.annotation.DubboService;
 import org.idea.live.user.constants.UserTagsEnum;
 import org.idea.live.user.interfaces.IUserTagRpc;
+import org.idea.live.user.provider.service.IUserTagService;
 
+import jakarta.annotation.Resource;
+
+@DubboService
 public class UserTagRpcImpl implements IUserTagRpc {
+    
+    @Resource
+    private IUserTagService userTagService;
 
     @Override
     public boolean setTag(Long userId, UserTagsEnum userTagsEnum) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setTag'");
+        return userTagService.setTag(userId, userTagsEnum);
     }
 
     @Override
     public boolean cancelTag(Long userId, UserTagsEnum userTagsEnum) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cancelTag'");
+        return userTagService.cancelTag(userId, userTagsEnum);
     }
 
     @Override
     public boolean containTag(Long userId, UserTagsEnum userTagsEnum) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'containTag'");
+        return userTagService.containTag(userId, userTagsEnum);
     }
     
 }
